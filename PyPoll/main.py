9a85fba58752a316132fe6ca44c072dca43f221c
CBPher24 <Christopher.b.partee@gmail.com>
2022-03-29 20:41:45 -0400

    added rolling building dictionary to go through ballots

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,11 +5,12 @@ csvpath = os.path.relpath("PyPoll/Resources/election_data.csv")
 
 #variables needed
 tot_votes = 0
-#running list of candidates
+#running dict of candidates
 candid = {}
-#tot_votes for each candidate
 #percentage of total votes for candidate
 n = 0
+
+
 
 #open and start read on csv
 with open(csvpath) as csvfile:
@@ -19,16 +20,20 @@ with open(csvpath) as csvfile:
     for row in elect:
     #running count of votes
         tot_votes = tot_votes + 1
+        person = row[2]
         #check to see if same candidate
-        if row[2] != candid.values()[n]:
-            candid.setdefault(row[2], []).append(row[2])
+        if person not in candid:
+            candid[person] = row
+        else:
+            candid[person].append(row)
         #running count of votes for candidate
 
 
-
+print(list(candid.keys()))
+print(len(candid["Raymon Anthony Doane"]))
 #print statement for terminal
-#print("Election Results\n------------------------")
-#print(f"Total Votes: {tot_votes}\n------------------------")
+print("Election Results\n------------------------")
+print(f"Total Votes: {tot_votes}\n------------------------")
 #all canidates, percentage of votes, and total votes for canidate
 #name: percentage% (total votes)
 
